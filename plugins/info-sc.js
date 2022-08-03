bbcf0fc047e68291297443d917a3a14b56b74b6a
koncit <tnnckl1553@gmail.com>
2022-08-04 01:54:27 +0700

    anyabotv3

---
+++ plugins/info-sc.js
@@ -0,0 +1,22 @@
+/*
+ * ngelarang lu biar g diubah kyknya emg susah ya anjg
+ * jadi kalau lu g bsa ngehargain orang
+ * setidaknya buat sc ori by gw, trs buat sc lu hasil copas tuh
+ * better bgtu dripada nma gw dihapus smua
+ * najis 👎🗿
+*/
+
+let handler = async (m, { conn }) => {
+let ye = `@${m.sender.split`@`[0]}`
+let esce = `
+Hai ${ye} Lagi Nyari Sc Ya? 
+
+
+_*https://www.youtube.com/channel/UClfUAjgtQ19vGnUbkQxTuAg*_\n_*Sewa Bot?*_\n_*https://wa.me/c/6281364444859*_`
+conn.sendBut(m.chat, esce, wm3, 'Thanks', 'thanks', m) 
+}
+handler.help = ['sc', 'sourcecode']
+handler.tags = ['info']
+handler.command = /^(sc|sourcecode)$/i
+
+module.exports = handler
---
+++ plugins/main-belibot.js
@@ -0,0 +1,26 @@
+let handler = async (m, { conn }) => {
+const ftroli = {
+    key : {
+    remoteJid: '6281364444859-5533146900058138@g.us',
+    participant : '0@s.whatsapp.net'
+    },
+    message: {
+    orderMessage: {
+    itemCount : 2022,
+    status: 1,
+    surface : 1,
+    message: 'Get Bot', 
+    orderTitle: `Koncit`,
+    thumbnail: 'https://i.ibb.co/F43D4cH/kannachan.jpg', 
+    sellerJid: '0@s.whatsapp.net' 
+    }
+    }
+    }
+	conn.reply(m.chat, price1, ftroli)
+}
+
+handler.help = ['sewa', 'sewabot']
+handler.tags = ['info']
+handler.command = /^(sewa|sewabot)?$/i
+
+module.exports = handler

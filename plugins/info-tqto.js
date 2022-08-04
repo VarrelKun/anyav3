3a78690686481ba3aab2a9449531348bdd9a9c06
K0NC1T <68502508+koncit@users.noreply.github.com>
2022-08-04 09:13:25 +0700

    Update info-tqto.js

--- plugins/info-tqto.js
+++ plugins/info-tqto.js
@@ -6,7 +6,7 @@
 
 let handler = async (m, { conn }) => {
 bear = "Source Code"
-let esce = 'BIG THANKS TO\n\nNurutomo\nhttps://github.com/Nurutomo\nGempy\nhttps://github.com/GempyTon\nHyzer Official\nhttps://github.com/Hyzerr'
+let esce = 'BIG THANKS TO\n\nRazul\nKoncit\nhttps://github.com/koncit'
 conn.sendButtonImg(m.chat, fla + 'Thanks To', esce, wm2, 'Menu', '.menu', m) 
 }
 handler.help = ['tqto', 'team']
